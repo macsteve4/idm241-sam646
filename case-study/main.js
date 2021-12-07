6c59f0937b28a44a87b97eaf2abf3bdff45e9010
macsteve4 <71797763+macsteve4@users.noreply.github.com>
2021-12-07 15:23:53 -0500

    Final polish edits
    Edits to polish GitHub repo as well as well as final layouts of sites.

---
+++ case-study/main.js
@@ -0,0 +1,21 @@
+var boingOne = $("#boingyoing")[0];
+		$(".onClickTextOverImage")
+	.mouseenter(function() {
+		boingOne.play();
+	});
+
+function StopSound() {
+  // Stop sound from playing
+  boingyoing.pause();
+  boingyoing.currentTime = 0;
+}
+
+function keepLocation(oldOffset) {
+	if (window.pageYOffset!= null){
+	  st=oldOffset;
+	}
+	if (document.body.scrollWidth!= null){
+	  st=oldOffset;
+	}
+	setTimeout('window.scrollTo(0,st)',1);
+  }
